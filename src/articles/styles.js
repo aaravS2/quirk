b72bd6500f1e0f5d1dd929fcc502d1a242226123
Akhliesh <varshha7@gmail.com>
2021-05-27 12:56:51 +0530

    modified files

---
+++ src/articles/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  text: {
+    color:"rgb(0, 255, 174)",
+    fontSize: 18,
+    lineHeight: 28,
+  },
+  heading: {
+    marginBottom: 5,
+    marginTop: 12,
+  },
+  heading1: {
+    fontSize: 20,
+    lineHeight: 22,
+    fontWeight: "900",
+  },
+  heading2: {
+    fontSize: 18,
+    lineHeight: 20,
+    fontWeight: "700",
+  },
+  pargaraph: {
+    marginTop: 0,
+  },
+});
